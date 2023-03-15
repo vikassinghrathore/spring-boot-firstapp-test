825272bdc3748c3912f4628dfcf9e01b1b6a3c8d
vikassinghrathore <vikaskanvika@gmail.com>
2023-03-15 17:08:49 +0530

    commit-1

---
+++ src/main/java/com/examples/ij/springbootfirstapptest/SpringBootFirstappTestApplication.java
@@ -0,0 +1,14 @@
+package com.examples.ij.springbootfirstapptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstappTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootFirstappTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examples/ij/springbootfirstapptest/SpringBootFirstappTestApplicationTests.java
@@ -0,0 +1,15 @@
+
+package com.examples.ij.springbootfirstapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstappTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
