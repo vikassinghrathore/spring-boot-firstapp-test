ccff1c8ea7b2258785f45e2ff28c23c3405740ef
vikassinghrathore <vikaskanvika@gmail.com>
2023-03-15 17:24:32 +0530

    added cgf

--- src/main/java/com/examples/ij/springbootfirstapptest/SpringBootFirstappTestApplication.java
+++ src/main/java/com/examples/ij/springbootfirstapptest/SpringBootFirstappTestApplication.java
@@ -2,13 +2,14 @@ package com.examples.ij.springbootfirstapptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstappTestApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(SpringBootFirstappTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext configurableApplicationContext =
+                SpringApplication.run(SpringBootFirstappTestApplication.class, args);
+    }
 
 }
